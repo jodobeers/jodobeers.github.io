adbd803873b7397c21b265b1692b88a34899859e
jodobeers <beer.jodocorporation@gmail.com>
2026-04-21 20:26:59 +0200

    Delete webapp directory

--- webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-QUnit.config.autostart = false;
-
-sap.ui.require(["alsm/dev/ui5/jodobeers/jodobeers/test/integration/AllJourneys"
-], function () {
-	QUnit.start();
-});
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"alsm/dev/ui5/jodobeers/jodobeers/test/unit/controller/App.controller"
-], function () {
-	"use strict";
-});
